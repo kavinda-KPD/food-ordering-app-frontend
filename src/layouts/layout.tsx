c217b2979ec9cf7533dd81c6e838ade67177a8b0
Kavinda Dissanayake <kavinda.dissanayake@aventagelabs.com>
2024-08-14 06:56:37 +0530

    initiate project

---
+++ src/layouts/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+type props = {
+    children: React.ReactNode
+}
+
+const layout = ({children}:props) => {
+
+    return (
+        <div className="flex flex-col min-h-screen">
+            
+        </div>
+    )
+}
+
+export default layout
