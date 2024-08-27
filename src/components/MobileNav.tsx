7fd6d148f81bb8a9354b9fe650c62ea725d56108
Kavinda Dissanayake <kavinda.dissanayake@aventagelabs.com>
2024-08-27 09:45:35 +0530

    front end header setup

---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,25 @@
+import { Menu } from "lucide-react"
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet"
+import { Button } from "./ui/button"
+
+const MobileNav = () => {
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Menu className="text-orange-500" />
+            </SheetTrigger>
+
+            <SheetContent className="space-y-3">
+                <SheetTitle>
+                    <span>Welcome to the Mern.resturent</span>
+                </SheetTitle>
+
+                <SheetDescription className="flex">
+                    <Button className="flex-1 font-bold bg-orange-500">Login</Button>
+                </SheetDescription>
+            </SheetContent>
+        </Sheet>
+    )
+}
+
+export default MobileNav
--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,16 +1,19 @@
+import Header from "@/components/Header"
 import React from "react"
 
 type props = {
     children: React.ReactNode
 }
 
-const layout = ({children}:props) => {
+const Layout = ({ children }: props) => {
 
     return (
         <div className="flex flex-col min-h-screen">
-            
+            <Header />
+
+            <div className="container mx-auto flex-1 py-10">{children}</div>
         </div>
     )
 }
 
-export default layout
+export default Layout
