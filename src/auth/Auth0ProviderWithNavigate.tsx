203d9cabf6992e19b11b8525a5cd86aab948f891
Kavinda Dissanayake <kavinda.dissanayake@aventagelabs.com>
2024-08-28 22:55:47 +0530

    create user from auth0

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,9 +1,15 @@
+import { useCreateMyUser } from "@/api/MyUserApi"
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react"
 
 type Props = {
     children: React.ReactNode
 }
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+
+    const { createUser } = useCreateMyUser() || {};
+    if(!createUser) {
+        throw new Error('Unable to initialize Auth0 provider')
+    }
 
     const domain = import.meta.env.VITE_AUTH0_DOMAIN
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
@@ -13,8 +19,15 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
         throw new Error('Unable to initialize Auth0 provider')
     }
 
-    const onRedirectCallback = (appState?: AppState , user?: User) => {
+    const onRedirectCallback = async (appState?: AppState, user?: User) => {
         console.log("User", user)
+
+        if (user?.sub && user?.email) {
+            createUser({
+                auth0Id: user.sub,
+                email: user.email
+            })
+        }
     }
 
     return (
